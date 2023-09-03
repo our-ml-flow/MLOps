cba0dc56f13d476a4d44222a453f741d2d372b97
jinlee057 <jinlee057@gmail.com>
2023-09-03 12:41:54 +0900

    feat: Update utils.py #4
    optimize query (cost decrease by 40% 15,040 -> 9095)

--- module/utils/utils.py
+++ module/utils/utils.py
@@ -7,22 +7,6 @@ import requests
 import pandas as pd
 
 
-
-@task(log_prints=True)
-def get_json_block(block_name: str, key: str) -> str|None:
-    api_key = None
-    
-    try:    
-        block = JSON.load(block_name)
-        
-        api_key = block.value[key]
-        
-    except Exception as e:
-        print(e.__doc__)
-        
-    finally:
-        return api_key
-    
 @task
 def get_raw_data(start_date, end_date):
     
@@ -30,22 +14,23 @@ def get_raw_data(start_date, end_date):
     connection = engine.connect()
 
     try:
-        query = f""" WITH owner_token_info as(
+        query = f""" WITH owner_token_info AS (
                         SELECT 
-                        owner_address,
-                        collection_name AS collection,
-                        num_distinct_tokens_owned AS num_token,
-                        sum(num_distinct_tokens_owned)over(PARTITION BY owner_address) AS tot,
-                        data_created_at::date AS created_at
+                            owner_address,
+                            collection_name AS collection,
+                            num_distinct_tokens_owned AS num_token,
+                            sum(num_distinct_tokens_owned) OVER (PARTITION BY owner_address) AS tot,
+                            data_created_at::date AS created_at
                         FROM alchemy_collection_for_buyer
-                        ORDER BY owner_address
-                        ) 
-                    SELECT owner_address, collection, 
-                    sum((num_token/tot)) AS token_ratio
-                    FROM owner_token_info 
-                    WHERE created_at BETWEEN '{start_date}' AND '{end_date}'
-                    GROUP BY owner_address, collection,created_at
-                    ORDER BY owner_address;"""
+                        WHERE data_created_at::date BETWEEN {start_date} AND {end_date}'
+                        )
+                        SELECT 
+                            owner_address, 
+                            collection,
+                            sum(num_token/tot) AS token_ratio
+                        FROM owner_token_info
+                        GROUP BY owner_address, collection, created_at
+                        ORDER BY owner_address;"""
         
         result = connection.execute(text(query))
         rows=result.fetchall()
